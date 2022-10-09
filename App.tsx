e6b6432a8866b7a604957cfc2161de20b9e8fc29
Santi-Leon146 <112590631+Santi-Leon146@users.noreply.github.com>
2022-10-09 20:03:03 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+//import logo from './logo.svg';import './App.css';
+import Nav from"./components/Navegacion/Navbar";
+import Header from"./components/header/header"
+import ItemListContainer from "./components/itemListConatainer/itemListContainer"
+//import ItemCount from "./components/itemCount/ItemCount"
+import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
+import "./Services/mockAPI" 
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Header/>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={
+         <ItemListContainer greeting="Tienda Luffy - Mangas"/>
+      }/>
+      <Route path="/Mangas/:itemid"element={
+        <ItemDetailContainer/>
+      }/>
+      <Route path="/category/:cat" element={
+        <ItemListContainer/>
+      }></Route>
+    
+     </Routes>
+     </BrowserRouter>
+
+    
+    </div>
+  );
+}
+
+export default App;
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-dtd47s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-dtd47s)
